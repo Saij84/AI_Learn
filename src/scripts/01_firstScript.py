2ea9fb733b607adc22372750d3f550cfb9789cd5
fangc <saij84@gmail.com>
2019-01-06 16:20:24 +1100

    first AI script

--- src/scripts/01_firstScript.py
+++ src/scripts/01_firstScript.py
@@ -6,6 +6,17 @@ https://www.youtube.com/watch?v=T5pRlIbr6gg&list=PL2-dafEMk2A6QKz1mrk1uIGfHkC1zZ
 from sklearn import tree
 
 #[height, weight, shoe size]
-x = [
+x = [[181,80,44], [177,70,43], [160,60,38], [154,54,37],
+     [166,65,40], [190,90,47], [175,64,39], [177,70,40],
+     [159,55,37], [171,75,42], [181,85,43]]
 
-]
+y = ['male', 'female', 'female', 'female', 'male', 'male',
+     'male', 'female', 'male', 'female', 'male']
+
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(x, y)
+
+prediction = clf.predict([[190,80,43]])
+
+print(prediction)
