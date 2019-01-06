5b533d49e3111153592e059fe0e83da8c1d85fe3
fangc <saij84@gmail.com>
2019-01-06 15:59:58 +1100

    first AI script

---
+++ src/scripts/01_firstScript.py
@@ -0,0 +1,11 @@
+"""
+Learn Python for Data Science #1
+https://www.youtube.com/watch?v=T5pRlIbr6gg&list=PL2-dafEMk2A6QKz1mrk1uIGfHkC1zZ6UU
+"""
+
+from sklearn import tree
+
+#[height, weight, shoe size]
+x = [
+
+]
