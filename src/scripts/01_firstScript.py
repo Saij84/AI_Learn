683abe0bef2629fdb04d4641f3ce91592a1a4752
fangc <saij84@gmail.com>
2019-01-07 21:04:51 +1100

    WIP

--- src/scripts/01_firstScript.py
+++ src/scripts/01_firstScript.py
@@ -4,19 +4,37 @@ https://www.youtube.com/watch?v=T5pRlIbr6gg&list=PL2-dafEMk2A6QKz1mrk1uIGfHkC1zZ
 """
 
 from sklearn import tree
+from sklearn import svm
 
-#[height, weight, shoe size]
-x = [[181,80,44], [177,70,43], [160,60,38], [154,54,37],
-     [166,65,40], [190,90,47], [175,64,39], [177,70,40],
-     [159,55,37], [171,75,42], [181,85,43]]
+#Data: [height, weight, shoe size]
+x = [[181,80,44], [177,70,43], [160,60,38], [154,54,37], [166,65,40],
+     [190,90,47], [175,64,39], [177,70,40], [159,55,37], [171,75,42], [181,85,43]]
 
-y = ['male', 'female', 'female', 'female', 'male', 'male',
-     'male', 'female', 'male', 'female', 'male']
+y = ['male', 'female', 'female', 'female', 'male', 'male', 'male', 'female', 'male', 'female', 'male']
+z = ['male', 'female', 'female', 'female', 'male', 'female', 'male', 'female', 'male', 'male', 'male']
 
-clf = tree.DecisionTreeClassifier()
 
-clf = clf.fit(x, y)
+#Classefiers
+clf_DTC = tree.DecisionTreeClassifier()
+clf_SVM = svm.SVC(gamma='auto')
 
-prediction = clf.predict([[190,80,43]])
+#Train model
+clf_DTC = clf_DTC.fit(x, y)
+clf_SVM = clf_SVM.fit(x, y)
 
-print(prediction)
+diff = list(set(y)-set(z))
+
+print(diff)
+
+"""
+#Test model
+index = 0
+for data in x:
+    prediction = clf_DTC.predict([data])
+    print("DTC Prediction: {}".format(prediction))
+print('\n')
+
+for data in x:
+    prediction = clf_SVM.predict([data])
+    print("SVM Prediction: {}".format(prediction))
+"""
