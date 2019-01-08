2d839f6e8db42400c82ac3d0d73f143a657a3d81
fangc <saij84@gmail.com>
2019-01-08 22:36:32 +1100

    excercise 02

--- src/scripts/02b_twitterSentiment.py
+++ src/scripts/02b_twitterSentiment.py
@@ -1,21 +1,31 @@
 import tweepy
 from textblob import TextBlob as tb
+import csv
 
-consumer_key = ""
-consumer_secret = ""
+consumer_key = "QfNCetLHJSayC8yC8YEbIQ8pd"
+consumer_secret = "haORWdlWc79Vnm7OAlWgqRnXdMd7hUweoiKa03PHNXUJBaUmfX"
 
-access_token = ""
-access_token_secret = ""
+access_token = "2998870596-7SM8anbzPdxoDTRkBYpWC1l5Xw05N24MxTestiN"
+access_token_secret = "LIxHQN42evo7Ch83CS6x7jIjrDSvoYyjBjDkuaMFa4RWN"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.search("finance")
 
-for tweet in public_tweets:
-    print(tweet.text)
-    analysis = tb(tweet.text)
-    print(analysis.sentiment)
+with open("02c_twitterSentiment.csv", "w", newline="") as csvfile:
+    tweetWriter = csv.writer(csvfile, delimiter=" ",
+                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+    public_tweets = api.search("trump", "eng")
+
+    for tweet in public_tweets:
+        inputText = tweet.text
+        print(inputText)
+
+        tweetWriter.writerow(inputText)
+
+        #analysis = tb(tweet.text)
+        #print(analysis.sentiment)
 
